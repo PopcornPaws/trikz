d0df51884b68c367be32ed1a2cfdaae7b6055b0f
PopcornPaws <lipschitz77@gmail.com>
2023-07-18 21:17:02 +0200

    Added example.

---
+++ examples/blockdiag.rs
@@ -0,0 +1,21 @@
+// TODO this is what I eventually want
+/*
+use trikz::{Rectangle, Circle, Arrow};
+
+fn main() {
+    // define styles
+    let plant = Rectangle::new();
+    let controller = Rectangle::new().at(plant.left(...));
+    let sum = Circle::new().at(controller.left(...));
+
+    let reference = Arrow::from(sum.left(xshift...)).to(sum.west()); // label
+    let error = Arrow::from(sum.east()).to(controller.west()); // label
+    let input = Arrow::from(controller.east()).to(plant.west()); // label
+    let output = Arrow::from(controller.east()).to(plant.west()); // label
+    let feedback = Arrow::from(...).down_left(...).left_up(...); // |__|
+
+    let document = SvgDocument::new();
+    // add elements with dedicated style to document
+    // export to svg
+}
+*/
